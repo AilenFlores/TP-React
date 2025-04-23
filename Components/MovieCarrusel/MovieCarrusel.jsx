5b2f73992f24102d900fd2c6c6a18c5f68a8106e
AilenFlores <ailen.flores@est.fi.uncoma.edu.ar>
2025-04-23 12:31:21 -0300

    Limpieza de código: eliminación de comentarios innecesarios y archivos SVG no utilizados, y ajustes menores en estilos CSS.

--- Components/MovieCarrusel/MovieCarrusel.jsx
+++ Components/MovieCarrusel/MovieCarrusel.jsx
@@ -6,22 +6,20 @@ const MovieCarousel = ({ movies, onMovieClick }) => {
 
   const scrollRef = useRef(null);
   const scrollLeft = () => {
-    scrollRef.current.scrollBy({ left: -280, behavior: 'smooth' }); // Cambia el valor de desplazamiento a la izquierda
+    scrollRef.current.scrollBy({ left: -280, behavior: 'smooth' }); 
   };
   const scrollRight = () => {
-    scrollRef.current.scrollBy({ left: 280, behavior: 'smooth' }); // Cambia el valor de desplazamiento a la derecha
+    scrollRef.current.scrollBy({ left: 280, behavior: 'smooth' });
   };
 
   return (
     <div className={Style.carouselWrapper}>
-      {/* Flecha izquierda visible al costado si hay 5+ pelis */}
       {movies.length > 4 && (
         <button className={`${Style.arrow} ${Style.left}`} onClick={scrollLeft}>
           &#8592;
         </button>
       )}
 
-      {/* Contenedor de tarjetas que tienen peliculas */}
       <div className={Style.cardContainer} ref={scrollRef}>
         {movies.map((movie) => (
           <MovieCard
@@ -32,7 +30,6 @@ const MovieCarousel = ({ movies, onMovieClick }) => {
         ))}
       </div>
 
-      {/* Flecha derecha visible al costado si hay 5+ pelis */}
       {movies.length > 4 && (
         <button className={`${Style.arrow} ${Style.right}`} onClick={scrollRight}>
           &#8594;
